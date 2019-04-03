7552c9512325c0edf7f7e3a8cf453bf733b9f856
Dennis Alund <dennis.alund@gmail.com>
2019-04-03 13:29:36 +0800

    Adding starter project

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+
+import * as tanam from 'tanam';
+
+tanam.initializeApp({
+    users: {
+        "your.email@gmail.com": "owner",
+    },
+    firebaseApp: {
+        apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        authDomain: "your-project-id.firebaseapp.com",
+        databaseURL: "https://your-project-id.firebaseio.com",
+        projectId: "your-project-id",
+        storageBucket: "your-project-id.appspot.com",
+        messagingSenderId: "0123456789"
+    },
+});
+
+export * from 'tanam';
